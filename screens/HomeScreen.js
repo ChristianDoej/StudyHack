06c655e982d00c68114ed01990058e40d5df89ad
Christian Brandborg <christianbrandborg@Christians-MacBook-Pro-2.local>
2018-11-10 13:14:59 +0100

    Home screen og FlatList oprettet

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,10 +5,13 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  FlatList,
   TouchableOpacity,
   View,
-  Button
+  Button,
+  ActivityIndicator
 } from 'react-native';
+import {ListItem} from 'react-native-elements';
 import { WebBrowser } from 'expo';
 import firebase from 'firebase';
 import { MonoText } from '../components/StyledText';
@@ -17,89 +20,76 @@ export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+constructor(props) {
+  super(props);
+  this.state = {
+    isLoading: true
+  }
+}
+  
 
+componentDidMount() {
+  this.readUserData();
+}
+
+readUserData() {
+
+  var that = this;
+
+  return firebase.database().ref('tutors').on('value', function (snapshot) {
+      var tutors = Object.values(snapshot.val());
+      //Brug artist ID til at hente fulde navn og erstat dataen. 
+      //Da dataen i øvelserne kun er fra Taylor Swift, går vi bare ind i første object i Arrayet, 
+      //da vi ved alle objekter har samme artist. Er der forskellige, kan man loope igennem arrayet og erstatte variabler
+      var tutorId = tutors[0].name;
+      
+      that.setState({
+        isLoading: false,
+        dataSource: tutors,
+      });
+
+      console.log(tutors)
+      
+    });
+}
+ 
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-          <View>
-          <Button title="Log Out" onPress={() => firebase.auth().signOut()}></Button>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, padding: 20, justifyContent: 'center', alignItems: 'stretch' }}>
+          <ActivityIndicator />
         </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+      )
     }
+  
+      return (
+        <View>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({ item }) =>
+            <ListItem
+              avatar={
+                <Image
+                  style={{ width: 65, height: 65 , borderRadius:30}}
+                  source={{ uri: item.image }} />
+              }
+              title={item.name}
+              titleStyle={{ color: 'tomato', fontWeight: 'bold' }}
+              subtitleStyle={{ color: 'tomato' }}
+              subtitle={item.price + " kr,- pr. time"}
+              rightTitle={item.course}
+              chevronColor='tomato'
+              onPress={() => this.props.navigation.navigate('Details', item)}
+              containerStyle={{ backgroundColor: 'white' }}
+            />
+          }
+          keyExtractor={(item, index) => index.toString()}
+        />
+        </View>
+    );
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,77 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { ActivityIndicator, FlatList, View, Image, Button } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import firebase from 'firebase';
+
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
-
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+  
+    static navigationOptions = {
+      title: 'Links',
+    };
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: true
+      }
+    }
+  
+    componentDidMount() {
+      this.getAlbumsFromApiAsync();
+    }
+  
+    getAlbumsFromApiAsync() {
+      return fetch('http://rallycoding.herokuapp.com/api/music_albums')
+        .then((response) => response.json())
+        .then((responseJson) => {
+  
+          this.setState({
+            isLoading: false,
+            dataSource: responseJson,
+          });
+  
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  
+    render() {
+      if (this.state.isLoading) {
+        return (
+          <View style={{ flex: 1, padding: 20, justifyContent: 'center', alignItems: 'stretch' }}>
+            <ActivityIndicator />
+          </View>
+        )
+      }
+  
+      return (
+          <View>
+            <Button title="Log ud" onPress={() => firebase.auth().signOut()}></Button>
+          
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={({ item }) =>
+              <ListItem
+                avatar={
+                  <Image 
+                  style={{width: 65, height: 65}} 
+                  source={{ uri: item.image }} />
+                }
+                title={item.title}
+                titleStyle={{ color: 'tomato', fontWeight: 'bold' }}
+                subtitleStyle={{ color: 'tomato' }}
+                subtitle={item.artist}
+                chevronColor='tomato'
+                onPress={() => this.props.navigation.navigate('Details', item)}
+                containerStyle={{backgroundColor: 'white'}}
+              />
+            }
+            keyExtractor={(item, index) => index.toString()}
+          />
+          </View>
+      );
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
